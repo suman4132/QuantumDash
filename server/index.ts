6d1d5c57eb58df19fef847d39ee071561e35e7f3
suman4132 <sk8760053@gmail.com>
2025-09-17 23:44:59 +0530

    Your commit message

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,9 @@
-import express, { type Request, Response, NextFunction } from "express";
 import { config } from "dotenv";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
-
 // Load environment variables
 config();
-
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "./routes";
+import { setupVite, serveStatic, log } from "./vite";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
